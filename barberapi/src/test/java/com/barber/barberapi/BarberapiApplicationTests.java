cc504bb1886bac371687fd45c0c6e909589740b1
Raul Melo <raulvitor123456@hotmail.com>
2025-03-16 03:26:47 -0300

    setup inicial

---
+++ barberapi/src/test/java/com/barber/barberapi/BarberapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barber.barberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
